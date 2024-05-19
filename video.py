1b33b8fabffd6853be1d3908884b09b3bbddd0be
shahriarXmode <137916535+shahriarXmode@users.noreply.github.com>
2024-05-19 08:23:08 -1200

    Update video.py

--- video.py
+++ video.py
@@ -1,17 +1,21 @@
 from pytube import YouTube
+import os
 
 def download_video(url, save_path):
     try:
         yt = YouTube(url)
         stream = yt.streams.get_highest_resolution()
-        stream.download(output_path=save_path)
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        video_file_path = os.path.join(save_path, yt.title + '.mp4')
+        stream.download(output_path=video_file_path)
         print("Download completed!")
     except Exception as e:
         print(f"Error downloading video: {e}")
 
 def main():
     video_url = input("Enter the YouTube video URL: ")
-    save_path = input("Enter the path to save the video: ")
+    save_path = "/home/shahriarmahmudoune150/youvid/video"  # Absolute path to the video folder
 
     download_video(video_url, save_path)
 
