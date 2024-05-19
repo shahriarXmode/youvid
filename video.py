7f7b233bf0c54501860bf144af6386e4cfeba1ee
shahriarXmode <137916535+shahriarXmode@users.noreply.github.com>
2024-05-19 09:17:13 -1200

    Update video.py

--- video.py
+++ video.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from pytube import YouTube
 import os
 
 app = Flask(__name__)
+
+DOWNLOAD_FOLDER = "/home/shahriarmahmudoune150/youvid/video"
+if not os.path.exists(DOWNLOAD_FOLDER):
+    os.makedirs(DOWNLOAD_FOLDER)
 
 @app.route('/')
 def index():
@@ -13,12 +17,17 @@ def download():
     video_url = request.form['url']
     yt = YouTube(video_url)
     stream = yt.streams.get_highest_resolution()
-    save_path = "video"
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-    video_file_path = os.path.join(save_path, yt.title + '.mp4')
-    stream.download(output_path=video_file_path)
-    return f"Download completed! Video saved as: {video_file_path}"
+    stream.download(output_path=DOWNLOAD_FOLDER)
+    return redirect(url_for('videos'))
+
+@app.route('/videos')
+def videos():
+    video_files = os.listdir(DOWNLOAD_FOLDER)
+    return render_template('videos.html', videos=video_files)
+
+@app.route('/video/<filename>')
+def video(filename):
+    return send_from_directory(DOWNLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
